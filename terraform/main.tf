7d98e2fc05888c6adcfc35d5708740be2d8ccd4e
impejonathan <impe.jonathan@gmail.com>
2025-02-26 15:44:30 +0100

    test verif rg + dl et autre  6

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,51 +4,92 @@ provider "azurerm" {
 
 # Vérification de l'existence du Resource Group
 data "azurerm_resource_group" "existing_rg" {
-  name = var.resource_group_name
+  count = var.check_existing ? 1 : 0
+  name  = var.resource_group_name
 }
 
-# Si le Resource Group n'existe pas, on le crée
+# Création du Resource Group seulement s'il n'existe pas
 resource "azurerm_resource_group" "rg" {
-  count    = length(data.azurerm_resource_group.existing_rg.id) > 0 ? 0 : 1
+  # Ne crée pas si le groupe existe déjà et que check_existing est true
+  count    = var.check_existing && length(data.azurerm_resource_group.existing_rg) > 0 ? 0 : 1
   name     = var.resource_group_name
   location = var.location
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# Référence au Resource Group (qu'il soit nouveau ou existant)
+locals {
+  resource_group_name = var.check_existing && length(data.azurerm_resource_group.existing_rg) > 0 ? data.azurerm_resource_group.existing_rg[0].name : azurerm_resource_group.rg[0].name
+  location            = var.check_existing && length(data.azurerm_resource_group.existing_rg) > 0 ? data.azurerm_resource_group.existing_rg[0].location : azurerm_resource_group.rg[0].location
 }
 
 # Vérification de l'existence du Storage Account
-data "azurerm_storage_account" "existing_sa" {
+data "azurerm_storage_account" "existing_datalake" {
+  count               = var.check_existing ? 1 : 0
   name                = var.storage_account_name
-  resource_group_name = var.resource_group_name
+  resource_group_name = local.resource_group_name
 }
 
-# Si le Storage Account n'existe pas, on le crée
+# Création du Storage Account seulement s'il n'existe pas
 resource "azurerm_storage_account" "datalake" {
-  count                             = length(data.azurerm_storage_account.existing_sa.id) > 0 ? 0 : 1
-  name                              = var.storage_account_name
-  resource_group_name               = var.resource_group_name
-  location                          = var.location
-  account_tier                      = "Standard"
-  account_replication_type          = "LRS"
-  is_hns_enabled                    = true
+  count                    = var.check_existing && length(data.azurerm_storage_account.existing_datalake) > 0 ? 0 : 1
+  name                     = var.storage_account_name
+  resource_group_name      = local.resource_group_name
+  location                 = local.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  is_hns_enabled           = true # Data Lake Gen2 activé
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
-# Création des Containers (Blobs) pour le Data Lake
+# Référence au Storage Account (qu'il soit nouveau ou existant)
+locals {
+  storage_account_id = var.check_existing && length(data.azurerm_storage_account.existing_datalake) > 0 ? data.azurerm_storage_account.existing_datalake[0].id : azurerm_storage_account.datalake[0].id
+}
+
+# Vérification de l'existence des containers
+data "azurerm_storage_container" "existing_bronze" {
+  count                = var.check_existing ? 1 : 0
+  name                 = "bronze-data"
+  storage_account_name = var.storage_account_name
+}
+
+data "azurerm_storage_container" "existing_datagouv" {
+  count                = var.check_existing ? 1 : 0
+  name                 = "data-gouv"
+  storage_account_name = var.storage_account_name
+}
+
+data "azurerm_storage_container" "existing_gold" {
+  count                = var.check_existing ? 1 : 0
+  name                 = "gold-data"
+  storage_account_name = var.storage_account_name
+}
+
+# Création des Containers seulement s'ils n'existent pas
 resource "azurerm_storage_container" "bronze-data" {
+  count                 = var.check_existing && length(data.azurerm_storage_container.existing_bronze) > 0 ? 0 : 1
   name                  = "bronze-data"
-  count                 = length(data.azurerm_storage_account.existing_sa.id) > 0 ? 1 : 0
-  storage_account_id    = data.azurerm_storage_account.existing_sa.id
+  storage_account_id    = local.storage_account_id
   container_access_type = "private"
 }
 
 resource "azurerm_storage_container" "data-gouv" {
+  count                 = var.check_existing && length(data.azurerm_storage_container.existing_datagouv) > 0 ? 0 : 1
   name                  = "data-gouv"
-  count                 = length(data.azurerm_storage_account.existing_sa.id) > 0 ? 1 : 0
-  storage_account_id    = data.azurerm_storage_account.existing_sa.id
+  storage_account_id    = local.storage_account_id
   container_access_type = "private"
 }
 
 resource "azurerm_storage_container" "gold-data" {
+  count                 = var.check_existing && length(data.azurerm_storage_container.existing_gold) > 0 ? 0 : 1
   name                  = "gold-data"
-  count                 = length(data.azurerm_storage_account.existing_sa.id) > 0 ? 1 : 0
-  storage_account_id    = data.azurerm_storage_account.existing_sa.id
+  storage_account_id    = local.storage_account_id
   container_access_type = "private"
 }
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,4 @@
+resource_group_name  = "RG-JIMPE-Certif"
+location             = "francecentral"
+storage_account_name = "dlcertifimpe"
+check_existing       = true
