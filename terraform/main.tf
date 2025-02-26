bb0e1770179bdfe125a63277ef944aefee542942
impejonathan <impe.jonathan@gmail.com>
2025-02-26 16:01:53 +0100

    test verif rg + dl et autre  9

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,84 +9,65 @@ data "azurerm_resource_group" "rg" {
 
 # Création du Resource Group seulement s'il n'existe pas déjà
 resource "azurerm_resource_group" "rg" {
-  # Création conditionnelle sans détruire l'existant
-  count    = try(data.azurerm_resource_group.rg.id, "") == "" ? 1 : 0
+  count    = data.azurerm_resource_group.rg.id == null ? 1 : 0
   name     = var.resource_group_name
   location = var.location
 }
 
-# Référence au Resource Group (qu'il soit existant ou créé)
+# Référence au Resource Group
 locals {
-  resource_group_name = try(data.azurerm_resource_group.rg.name, try(azurerm_resource_group.rg[0].name, var.resource_group_name))
-  location            = try(data.azurerm_resource_group.rg.location, try(azurerm_resource_group.rg[0].location, var.location))
+  resource_group_id   = coalesce(try(data.azurerm_resource_group.rg.id, null), try(azurerm_resource_group.rg[0].id, null))
+  resource_group_name = var.resource_group_name
+  location            = var.location
 }
 
-# Data source pour vérifier l'existence du Storage Account
-data "azurerm_storage_account" "datalake" {
-  name                = var.storage_account_name
-  resource_group_name = local.resource_group_name
-  depends_on          = [azurerm_resource_group.rg]
-}
-
-# Création du Storage Account seulement s'il n'existe pas
+# Création du Storage Account - sans vérification conditionnelle
 resource "azurerm_storage_account" "datalake" {
-  count                    = try(data.azurerm_storage_account.datalake.id, "") == "" ? 1 : 0
   name                     = var.storage_account_name
   resource_group_name      = local.resource_group_name
   location                 = local.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   is_hns_enabled           = true
+
+  # Utiliser cette ligne si vous voulez empêcher la destruction
+  lifecycle {
+    prevent_destroy = true
+    # Cette option ignore les changements sur ces attributs
+    ignore_changes = [
+      tags,
+    ]
+  }
 }
 
-# Référence au Storage Account
-locals {
-  storage_account_id = try(data.azurerm_storage_account.datalake.id, try(azurerm_storage_account.datalake[0].id, ""))
-}
-
-# Vérification si les containers existent déjà
-data "azurerm_storage_container" "bronze" {
-  count                = 1
-  name                 = "bronze-data"
-  storage_account_name = var.storage_account_name
-  depends_on           = [data.azurerm_storage_account.datalake, azurerm_storage_account.datalake]
-}
-
-data "azurerm_storage_container" "datagouv" {
-  count                = 1
-  name                 = "data-gouv"
-  storage_account_name = var.storage_account_name
-  depends_on           = [data.azurerm_storage_account.datalake, azurerm_storage_account.datalake]
-}
-
-data "azurerm_storage_container" "gold" {
-  count                = 1
-  name                 = "gold-data"
-  storage_account_name = var.storage_account_name
-  depends_on           = [data.azurerm_storage_account.datalake, azurerm_storage_account.datalake]
-}
-
-# Création des containers s'ils n'existent pas déjà
+# Création des containers de stockage
 resource "azurerm_storage_container" "bronze-data" {
-  count                 = try(data.azurerm_storage_container.bronze[0].id, "") == "" ? 1 : 0
   name                  = "bronze-data"
-  storage_account_id    = local.storage_account_id
+  storage_account_id    = azurerm_storage_account.datalake.id
   container_access_type = "private"
-  depends_on            = [azurerm_storage_account.datalake]
+
+  # Empêcher la destruction
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "azurerm_storage_container" "data-gouv" {
-  count                 = try(data.azurerm_storage_container.datagouv[0].id, "") == "" ? 1 : 0
   name                  = "data-gouv"
-  storage_account_id    = local.storage_account_id
+  storage_account_id    = azurerm_storage_account.datalake.id
   container_access_type = "private"
-  depends_on            = [azurerm_storage_account.datalake]
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "azurerm_storage_container" "gold-data" {
-  count                 = try(data.azurerm_storage_container.gold[0].id, "") == "" ? 1 : 0
   name                  = "gold-data"
-  storage_account_id    = local.storage_account_id
+  storage_account_id    = azurerm_storage_account.datalake.id
   container_access_type = "private"
-  depends_on            = [azurerm_storage_account.datalake]
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
