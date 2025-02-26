c5cbae281c5f27996360affa46cde964237aabc5
impejonathan <impe.jonathan@gmail.com>
2025-02-26 14:22:43 +0100

    7eme  push terraform azure create RG

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,5 +13,5 @@ variable "location" {
 variable "storage_account_name" {
   description = "Nom du Storage Account"
   type        = string
-  default     = "datalakecertifimpe"
+  default     = "dlcertifimpe"
 }
