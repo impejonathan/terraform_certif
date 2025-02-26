46018dc3c38ac8d7eb7a74b6dd54f888ee6ec8da
impejonathan <impe.jonathan@gmail.com>
2025-02-26 13:47:35 +0100

    2eme  push terraform azure create RG

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-state-impe-jonathan"
-    storage_account_name = "saterraformstate-impe-jonathan"
+    resource_group_name  = "rg-terraform-state-impe"
+    storage_account_name = "satfstateimpe" 
     container_name       = "tfstate-impe-jonathan"
     key                  = "prod.terraform.tfstate"
   }
